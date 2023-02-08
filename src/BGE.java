9a1f10d8b8502adf3d7a15bd8d48b9519c71e703
joanna <joannaelia258@gmail.com>
2023-02-08 10:21:31 -0600

    initial commit of bge file with method headers to be implemented

---
+++ src/BGE.java
@@ -0,0 +1,12 @@
+public class BGE {
+    /**
+     * Battleship game engine or BGE implements a one player battleship game as a package
+     */
+    public void startGame(){
+
+    }
+    public void shoot(int r, int c){
+
+    }
+
+}
