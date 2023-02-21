2727f93303630ecff9c095f11866164e00c05313
joanna <joannaelia258@gmail.com>
2023-02-21 13:15:57 -0600

    trying some things out

--- src/BattleshipGUI.java
+++ src/BattleshipGUI.java
@@ -1,2 +1,38 @@
-public class BattleshipGUI {
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class BattleshipGUI extends JPanel{
+	public JLabel labelText;
+	public JButton btn;
+	
+	public BattleshipGUI() {
+		JFrame f = new JFrame("test");
+		f.setVisible(true);
+		f.setSize(500,500);
+		f.setLayout(null);
+		labelText = new JLabel("StackOverflow");
+		labelText.setBounds(50, 100, 150, 30);
+		f.add(labelText);
+		
+		btn = new JButton("Change Text");
+		btn.setBounds(70, 200, 150, 60);
+		f.add(btn);
+		
+		
+		btn.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				btn.setText("Changed");
+			}
+		});
+	}
+	
+	public static void main(String[] args) {
+		new BattleshipGUI();
+	}
+
 }
